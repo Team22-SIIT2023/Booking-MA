49a05b911ba2168fc8eb44dbeb3b5c5d69b526a2
Sonja Baljicki <sonjabaljicki2002@gmail.com>
2023-11-08 21:08:43 +0100

    added list with existing accomodations for guests

--- app/src/main/java/com/example/booking_team22/fragments/accomodation/GuestAccomodationPageFragment.java
+++ app/src/main/java/com/example/booking_team22/fragments/accomodation/GuestAccomodationPageFragment.java
@@ -2,6 +2,7 @@ package com.example.booking_team22.fragments.accomodation;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.booking_team22.R;
+import com.example.booking_team22.databinding.FragmentAccomodationPageBinding;
+import com.example.booking_team22.fragments.FragmentTransition;
+import com.example.booking_team22.model.Accomodation;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +23,57 @@ import com.example.booking_team22.R;
  */
 public class GuestAccomodationPageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static ArrayList<Accomodation> products = new ArrayList<Accomodation>();
+    //private ProductsPageViewModel productsViewModel;
+    private FragmentAccomodationPageBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public GuestAccomodationPageFragment() {
-        // Required empty public constructor
+    public static GuestAccomodationPageFragment newInstance() {
+        return new GuestAccomodationPageFragment();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccomodationPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GuestAccomodationPageFragment newInstance(String param1, String param2) {
-        GuestAccomodationPageFragment fragment = new GuestAccomodationPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        products = new ArrayList<Accomodation>();
+        binding = FragmentAccomodationPageBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        prepareProductList(products);
+
+        FragmentTransition.to(GuestAccomodationListFragment.newInstance(products), getActivity(), false, R.id.scroll_products_list);
+
+        return root;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accomodation_page, container, false);
+    private void prepareProductList(ArrayList<Accomodation> products){
+        products.add(new Accomodation(
+                1L,
+                "Accomodation name",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                R.drawable.ap1));
+        products.add(new Accomodation(
+                2L,
+                "Accomodation name",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                R.drawable.ap2));
+        products.add(new Accomodation(
+                3L,
+                "Accomodation name",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                R.drawable.ap4));
+        products.add(new Accomodation(
+                4L,
+                "Accomodation name",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                R.drawable.ap5));
+        products.add(new Accomodation(
+                5L,
+                "Accomodation name",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                R.drawable.ap6));
     }
 }
