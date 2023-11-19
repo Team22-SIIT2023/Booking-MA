3878a19472f8575877605eef4235156d0770e780
VanjaJe <jevtic.sv11.2021@uns.ac.rs>
2023-11-19 15:02:23 +0100

    GUI fixing

--- app/src/main/java/com/example/booking_team22/activities/RegisterActivity.java
+++ app/src/main/java/com/example/booking_team22/activities/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.booking_team22.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.booking_team22.databinding.ActivityRegisterBinding;
 import com.example.booking_team22.databinding.ActivitySplashScreenBinding;
@@ -15,6 +17,15 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_register);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        String[] arraySpinner = new String[] {
+                "Guest", "Host"
+        };
+        Spinner s = (Spinner) binding.spinner;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
         setContentView(binding.getRoot());
     }
 }
